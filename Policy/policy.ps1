b19cee1b56ec9d4a022975961ab3d1a6cbd71f24
Marc van Eijk <marcvaneijk@msn.com>
2017-06-18 16:51:10 +0200

    Imamges and fix

--- Policy/policy.ps1
+++ Policy/policy.ps1
@@ -1,4 +1,6 @@
-﻿# Login to Azure
+﻿### Task 1: Create an Azure resource group and deploy and ARM template ###
+
+# Login to Azure
 Login-AzureRmAccount
 $s = Select-AzureRmSubscription -SubscriptionName "[Your subscription name]"
 
@@ -7,12 +9,22 @@ $resourceGroup = New-AzureRmResourceGroup -Name "AzureStack" -Location "West US"
 $resourceGroupName = $resourceGroup.ResourceGroupName
 
 # Deploy ARM Template
-New-AzureRmResourceGroupDeployment -Name "AzureOnly" -ResourceGroupName $resourceGroupName -TemplateUri https://raw.githubusercontent.com/Azure/AzureStack-Labs/master/Policy/azuredeploy.json -Verbose
+Try {
+New-AzureRmResourceGroupDeployment -Name "NoPolicy" `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateUri https://raw.githubusercontent.com/Azure/AzureStack-Labs/master/Policy/azuredeploy.json `
+    -Verbose -ErrorAction Stop
+}
+Catch {
+Write-Host $_.Exception.Message -ForegroundColor Red
+}
+
+### Task 2: Use the Azure Stack Policy module to constrain the resource group ###
 
 # Download Azure Stack Tools
 Invoke-WebRequest https://github.com/Azure/AzureStack-Tools/archive/master.zip -OutFile master.zip
-Expand-Archive master.zip -DestinationPath c:\ -Force
-cd c:\AzureStack-Tools-master
+Expand-Archive master.zip -DestinationPath \. -Force
+cd \AzureStack-Tools-master
 
 # Import policy module
 import-module .\Policy\AzureStack.Policy.psm1
@@ -23,12 +35,32 @@ $policy = New-AzureRmPolicyDefinition -Name AzureStackPolicy -Policy (Get-AzureS
 # Assign the new policy to the resource group
 New-AzureRmPolicyAssignment -Name AzureStackPolicy -PolicyDefinition $policy -Scope $ResourceGroup.ResourceId
 
+### Task 3: Test the limits of the constrained resource group ###
+
 # Deploy the ARM Template
-New-AzureRmResourceGroupDeployment -Name "AzureOnly" -ResourceGroupName $rgName -TemplateUri https://raw.githubusercontent.com/Azure/AzureStack-Labs/master/Policy/azuredeploy.json -Verbose
+Try {
+New-AzureRmResourceGroupDeployment -Name "Policy" `
+    -ResourceGroupName $resourceGroup.ResourceGroupName `
+    -TemplateUri https://raw.githubusercontent.com/Azure/AzureStack-Labs/master/Policy/azuredeploy.json `
+    -Verbose -ErrorAction Stop
+}
+Catch {
+Write-Host $_.Exception.Message -ForegroundColor Red
+}
+
+### Task 4: Update the template for Azure Stack ###
 
 # Download a local copy of the ARM Template for updating
 $localTemplate = "c:\temp\azuredeploy.json"
 Invoke-RestMethod https://raw.githubusercontent.com/Azure/AzureStack-Labs/master/Policy/azuredeploy.json -OutFile $localTemplate
 
 # Deploy the ARM Template after updating
-New-AzureRmResourceGroupDeployment -Name "AzureAndAzureStack" -ResourceGroupName $rgName -TemplateFile $localTemplate -Verbose
+Try {
+New-AzureRmResourceGroupDeployment -Name "Fixed" `
+    -ResourceGroupName $resourceGroup.ResourceGroupName `
+    -TemplateFile $localTemplate `
+    -Verbose -ErrorAction Stop
+}
+Catch {
+Write-Host $_.Exception.Message -ForegroundColor Red
+}
