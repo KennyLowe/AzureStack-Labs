ea97474254220719e7b1e52001d2216fb6a42460
Heath Lawson <helaw@microsoft.com>
2017-07-14 13:25:18 -0700

    Updating format

---
+++ Validate Templates/validate.ps1
@@ -0,0 +1,6 @@
+md c:\AzureStack_Labs\ValidateTemplates
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/Azure/AzureStack-Labs/master/Policy/azuredeploy.json" `
+-OutFile c:\AzureStack_Labs\ValidateTemplates\azuredeploy.json
+
+
+
