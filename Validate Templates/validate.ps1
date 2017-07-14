39d55da0f1fed7ca99fd5188c9c2d7b5c9a468c7
Heath Lawson <helaw@microsoft.com>
2017-07-14 13:56:14 -0700

    Updating validator

--- Validate Templates/validate.ps1
+++ Validate Templates/validate.ps1
@@ -1,12 +1,15 @@
+#Template validator lab
+
+# Download the template.
 md c:\AzureStack_Labs\ValidateTemplates
 Invoke-WebRequest -uri "https://raw.githubusercontent.com/Azure/AzureStack-Labs/master/Validate%20Templates/azuredeploy.json" `
 -OutFile c:\AzureStack_Labs\ValidateTemplates\azuredeploy.json
 
-
+# Change to the Template Validator directory and import the module
 cd \AzureStack-Tools-master\TemplateValidator
 Import-Module .\TemplateValidator\AzureRM.TemplateValidator.psm1
 
-
+#Run the template validator against the template.  This will log to the console and also create a report file.
 Test-AzureRMTemplate -TemplatePath c:\AzureStack_Labs\ValidateTemplates\azuredeploy.json `
 -CapabilitiesPath AzureStackCloudCapabilities_with_AddOns_20170627.json `
 -Verbose
